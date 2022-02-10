a3f43f9f449eacab14df1c3042f241dc9cdea139
jeffrey-pinyan-cleandns <jeffrey.pinyan@cleandns.email>
2022-02-10 14:02:11 -0500

    Updates

---
+++ precache-manifest.621439df4e0112faaba99ed40458508a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b805f7191d074a1b0912bdcaac8490a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51f53d1c352e263a9bc6",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "247d629d64553d180aa8",
+    "url": "./static/js/2.ac0e5440.chunk.js"
+  },
+  {
+    "revision": "697503643580ca28ddbc8f0a1738cdb1",
+    "url": "./static/js/2.ac0e5440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f53d1c352e263a9bc6",
+    "url": "./static/js/main.615f16df.chunk.js"
+  },
+  {
+    "revision": "582f1163f8951429701c",
+    "url": "./static/js/runtime-main.25420572.js"
+  }
+]);
